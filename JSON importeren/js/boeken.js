5d9ced276ea535078566e8a246835adec03225f6
Kamil38 <55272106+Kamil38@users.noreply.github.com>
2020-10-28 13:00:24 +0100

    video 3 afgerond

--- JSON importeren/js/boeken.js
+++ JSON importeren/js/boeken.js
@@ -4,9 +4,22 @@ const xhr = new XMLHttpRequest();
 xhr.onreadystatechange = () => {
     if(xhr.readyState == 4 && xhr.status == 200) {
         let resultaat = JSON.parse(xhr.responseText);
-        console.log(resultaat);
+        boeken.data = resultaat
+        boeken.uitvoeren();
     }
 }
 xhr.open('GET', 'boekenKamil.json', true);
 xhr.send();
+
+const boeken = {
+
+    //er wordt hier een eigenschap data aangemaakt (regel 7)
+    uitvoeren() {
+        let html = "";
+        this.data.forEach( boek => {
+            html += `<h3>${boek.titel}</h3>`
+        });
+        uitvoer.innerHTML = html
+    }
+}
 
