a890bc8042e9e7f4f3384c2962fbf59051b1ddf3
Kamil38 <55272106+Kamil38@users.noreply.github.com>
2020-11-10 14:13:37 +0100

    Video 13 afgerond

--- JSON importeren/js/boeken.js
+++ JSON importeren/js/boeken.js
@@ -13,11 +13,19 @@ xhr.send();
 
 const boeken = {
 
-    taalFilter: 'Nederlands',
+    taalFilter: ['Nederlands', 'Duits', 'Engels'],
 
     //filteren op taal
     filteren( gegevens ) {
-        this.data = gegevens.filter( (bk) => { return bk.taal == this.taalFilter} );
+        this.data = gegevens.filter( (bk) => {
+            let bool = false;
+                this.taalFilter.forEach( (taal) => {
+                    if( bk.taal == taal ) {
+                        bool = true
+                    }
+                } )
+            return bool;
+        } )
     },
 
     //er wordt hier een eigenschap data aangemaakt (regel 7)
